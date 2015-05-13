e20b6790808406ef612d4097bdbf3ac0f2230b8c
Brennen Peters <brennen.peters@civitaslearning.com>
2015-05-12 20:51:01 -0500

    npm init

---
+++ api/index.js
@@ -0,0 +1,32 @@
+var Parse = require('parse').Parse;
+var config = require('config');
+var parseApp = process.env.PARSE_APP || config.get('Parse.app');
+var parseJavascript = process.env.PARSE_JAVASCRIPT || config.get('Parse.javascript');
+
+Parse.initialize(parseApp, parseJavascript);
+
+var Scout = Parse.Object.extend("Scout");
+
+exports.postScouts = function(req, res) {
+
+	var scout = new Scout();
+	var point = new Parse.GeoPoint(req.query.position);
+
+	scout.set("location", point);
+
+	scout.save(null, {
+		success: function(scout) {
+			res.json(scout);
+		},
+		error: function(scout, error) {
+			res.send(error);
+		}
+	});
+
+};
+
+exports.getScouts = function(req, res) {
+
+	res.json({res: res.query});
+
+};
